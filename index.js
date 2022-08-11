c4daf17eed3bdb286895d0f55717b201cdd29cfe
gutche <gledrianregalagutierrez@gmail.com>
2022-08-11 14:15:08 +0200

    request header parser

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  const language = req.get('Accept-Language');
+  const software = req.get('user-agent');
+  res.json({ ipaddress, language, software });
 });
 
 // listen for requests :)
